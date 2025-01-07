3ac48ab4da8e214a45738c0cfbf941f69f87fff5
Akiva Bloch <akiva@kubiya.ai>
2025-01-07 17:40:01 +0200

    simplify

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,5 @@
-provider "aws" {
-  region = "us-east-1"
-  profile = "kubiya"
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "akivalue-test-bucket"
-  tags = {
-    Name        = "example-bucket"
-    Environment = "Dev"
+output "test" {
+  value = {
+    "test" = var.test
   }
-}
-
-resource "local_file" "example" {
-  content = var.test
-  filename = "${path.module}/example.txt"
-}
-
-resource "aws_s3_object" "example" {
-  bucket       = aws_s3_bucket.example.bucket
-  key          = "example.txt"
-  source       = local_file.example.filename
-  content_type = "text/plain"
-  etag = md5(var.test)
 }
